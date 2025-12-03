ad824bd24d8d6b2dd792f891e6b6499184526ffa
felipe.cardona <felipe.cardona@ceiba.com.co>
2025-12-03 18:24:04 -0500

    Feat: add debounce in search

--- country-app/src/app/country/components/search-input/search-input.ts
+++ country-app/src/app/country/components/search-input/search-input.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -8,8 +8,26 @@ import { Component, input, output } from '@angular/core';
 export class SearchInput {
   placeholder = input<string>('Buscar país por capital');
   searchValue = output<string>();
+  debounceTime = signal<number>(1000);
+  initialValue = input<string>();
+
+  inputValue = signal<string>('');
+
   onSearchValue(value: string) {
     if (!value || value.length === 0) return;
     this.searchValue.emit(value);
   }
+
+  debounceEffect = effect((onCleanup) => {
+    const value = this.inputValue();
+
+    const timeout = setTimeout(() => {
+      console.log('Input Value:', value);
+      return this.searchValue.emit(value);
+    }, this.debounceTime());
+
+    onCleanup(() => {
+      clearTimeout(timeout);
+    });
+  });
 }
