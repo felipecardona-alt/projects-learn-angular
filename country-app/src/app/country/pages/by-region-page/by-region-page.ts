f7207579a9568bd20e1b7559fae8b935ef94e8e7
felipe.cardona <felipe.cardona@ceiba.com.co>
2025-12-05 09:30:26 -0500

    Feat: add logic in region component

--- country-app/src/app/country/pages/by-region-page/by-region-page.ts
+++ country-app/src/app/country/pages/by-region-page/by-region-page.ts
@@ -1,7 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CountryList } from "../../components/country-list/country-list";
-import { RestCountry } from '../../interfaces/rest-countries';
-import { Country } from '../../interfaces/country';
+import { Region } from '../../interfaces/region';
+import { CountryServices } from '../../services/country.service';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'by-region-page',
@@ -9,5 +11,36 @@ import { Country } from '../../interfaces/country';
   templateUrl: './by-region-page.html',
 })
 export class ByRegionPage {
-  countries = signal<Country[]>([]);
+  countryServices = inject(CountryServices);
+  query = signal<Region | ''>('');
+  public regions: Region[] = [
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania',
+    'Antarctic',
+  ];
+
+  private buttonClasses: string[] = [
+    'btn-primary',
+    'btn-secondary',
+    'btn-accent',
+    'btn-info',
+    'btn-success',
+    'btn-warning'
+  ];
+
+  getButtonClass(index: number): string {
+    return this.buttonClasses[index % this.buttonClasses.length];
+  }
+
+  countryResource = rxResource({
+    params: () => ({ query: this.query() }),
+    stream: ({params}) => {
+      if (!params.query ) return of([]);
+
+      return this.countryServices.searchByRegion(params.query)
+    },
+  })
 }
--- country-app/src/app/country/services/country.service.ts
+++ country-app/src/app/country/services/country.service.ts
@@ -2,21 +2,32 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environments';
 import { RestCountry } from '../interfaces/rest-countries';
-import { catchError, delay, map, throwError } from 'rxjs';
+import { catchError, delay, map, Observable, of, tap, throwError } from 'rxjs';
 import { CountryMapper } from '../mapper/country.mapper';
+import { Country } from '../interfaces/country';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryServices {
   private http = inject(HttpClient);
+  private queryCacheByCapital = new Map<string, Country[]>();
+  private queryCacheByCountry = new Map<string, Country[]>();
+  private queryCacheByRegion = new Map<string, Country[]>();
 
-  searchByCapital(capital: string) {
+  searchByCapital(capital: string): Observable<Country[]> {
     capital = capital.trim().toLocaleLowerCase();
+
+    if (this.queryCacheByCapital.has(capital)) {
+      return of(this.queryCacheByCapital.get(capital) ?? []);
+    }
+
+    console.log('Fetching from API for capital:', capital, this.queryCacheByCapital);
 
     return this.http.get<RestCountry[]>(`${environment.apiBaseUrl}/capital/${capital}`)
     .pipe(
-      map((countries ) => CountryMapper.mapRestCountriesToCountries(countries)),
+      map((countries) => CountryMapper.mapRestCountriesToCountries(countries)),
+      tap( countries => this.queryCacheByCapital.set(capital, countries)),
       catchError(error => {
         console.error('Error fetching countries by capital:', error);
         return throwError(() => new Error(`No se pudo encontrar paises con la capital ${capital}`));
@@ -27,9 +38,14 @@ export class CountryServices {
   searchByCountry(code: string) {
     code = code.trim().toLocaleLowerCase();
 
+    if (this.queryCacheByCountry.has(code)) {
+      return of(this.queryCacheByCountry.get(code) ?? []);
+    }
+
     return this.http.get<RestCountry[]>(`${environment.apiBaseUrl}/name/${code}`)
     .pipe(
       map((resp ) => CountryMapper.mapRestCountriesToCountries(resp)),
+      tap( countries => this.queryCacheByCountry.set(code, countries)),
       delay(1500),
       catchError(error => {
         console.error('Error fetching countries by alpha code:', error);
@@ -47,6 +63,18 @@ export class CountryServices {
       catchError(error => {
         console.error('Error fetching countries by alpha code:', error);
         return throwError(() => new Error(`No se pudo encontrar detalles del pais con el codigo ${code}`));
+      })
+    );
+  }
+
+  searchByRegion(region: string): Observable<Country[]> {
+    return this.http.get<RestCountry[]>(`${environment.apiBaseUrl}/region/${region}`)
+    .pipe(
+      map((countries) => CountryMapper.mapRestCountriesToCountries(countries)),
+      tap( countries => this.queryCacheByRegion.set(region, countries)),
+      catchError(error => {
+        console.error('Error fetching countries by region:', error);
+        return throwError(() => new Error(`No se pudo encontrar paises en la region ${region}`));
       })
     );
   }
