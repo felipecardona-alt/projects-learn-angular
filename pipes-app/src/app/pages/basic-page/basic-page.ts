2ddbac2c0a756ff925d7895400382154af909eb3
felipe.cardona <felipe.cardona@ceiba.com.co>
2025-12-09 17:13:56 -0500

    Feat(pipes-app): ✨ add basic pipe page

--- pipes-app/src/app/pages/basic-page/basic-page.ts
+++ pipes-app/src/app/pages/basic-page/basic-page.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'basic-page',
-  imports: [],
+  imports: [
+    LowerCasePipe, UpperCasePipe, TitleCasePipe
+  ],
   templateUrl: './basic-page.html',
 })
-export default class BasicPage { }
+export default class BasicPage {
+  nameLower = signal('Felipe');
+  nameUpper = signal('cardona');
+  fullName = signal('FeLiPe CaRdOnA');
+}
