51c6ef8f31a0e1e02b8bd49a52d563a02dabd51a
felipe.cardona <felipe.cardona@ceiba.com.co>
2025-12-09 18:05:25 -0500

    Feat(pipes-app): ✨ add date pipes

--- pipes-app/src/app/pages/basic-page/basic-page.ts
+++ pipes-app/src/app/pages/basic-page/basic-page.ts
@@ -1,10 +1,10 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'basic-page',
   imports: [
-    LowerCasePipe, UpperCasePipe, TitleCasePipe
+    LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe
   ],
   templateUrl: './basic-page.html',
 })
@@ -12,4 +12,14 @@ export default class BasicPage {
   nameLower = signal('Felipe');
   nameUpper = signal('cardona');
   fullName = signal('FeLiPe CaRdOnA');
+
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanup) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+    }, 1000);
+
+    onCleanup(() => clearInterval(interval));
+  });
 }
