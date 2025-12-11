5e31a994cca7d034238eac9f5b71d2d288a28a5f
felipe.cardona <felipe.cardona@ceiba.com.co>
2025-12-11 11:57:39 -0500

    Feat(pipes-app): ✨ add i8nPluralPipe

--- pipes-app/src/app/pages/uncommon-page/uncommon-page.ts
+++ pipes-app/src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,7 +1,7 @@
 import { Person } from './../../interfaces/person';
 import { Component, signal } from '@angular/core';
 import { Card } from '../../components/card/card';
-import { I18nSelectPipe } from '@angular/common';
+import { I18nPluralPipe, I18nSelectPipe } from '@angular/common';
 
 const clientMale: Person = {
   name: 'Felipe',
@@ -19,11 +19,11 @@ const clientFemale: Person = {
 
 @Component({
   selector: 'uncommon-page',
-  imports: [Card, I18nSelectPipe],
+  imports: [Card, I18nSelectPipe, I18nPluralPipe],
   templateUrl: './uncommon-page.html',
 })
 export default class UncommonPage {
-
+  // i18nSelect
   client = signal<Person>(clientMale);
 
   invitationMap = {
@@ -37,6 +37,19 @@ export default class UncommonPage {
       return;
     }
     this.client.set(clientMale);
+  }
+
+  // i18nPlural
+  clients = signal<string[]>(['Maria', 'Pedro', 'Juan', 'Ana', 'Luis', 'Carmen']);
+  clientsMap = signal({
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos dos clientes esperando.',
+    other: 'tenemos # clientes esperando.'
+  });
+
+  deleteClient() {
+    this.clients.update(currentClients => currentClients.slice(1));
   }
 
 }
