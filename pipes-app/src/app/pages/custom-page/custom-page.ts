fe1532095fdddbe0015cdcb8a6fd8590c9be0248
felipe.cardona <felipe.cardona@ceiba.com.co>
2025-12-11 14:24:21 -0500

    Feat(pipes-app): ✨ add structure by custom pipes

--- pipes-app/src/app/pages/custom-page/custom-page.ts
+++ pipes-app/src/app/pages/custom-page/custom-page.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { SearchBoxComponent } from '../../components/search-box/search-box';
 
 @Component({
   selector: 'custom-page',
-  imports: [SearchBoxComponent],
+  imports: [],
   templateUrl: './custom-page.html',
 })
 export default class CustomPage { }
