e38dff494c05cedbfc1c3e22e8822866915110ca
felipe.cardona <felipe.cardona@ceiba.com.co>
2025-12-22 10:02:58 -0500

    Feat(basic-page): ✨ implement reactive form wit FormGroup

--- forms-app/src/app/reactive/pages/basic-page/basic-page.ts
+++ forms-app/src/app/reactive/pages/basic-page/basic-page.ts
@@ -1,9 +1,16 @@
 import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'basic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.html',
 })
-export class BasicPage { }
+export class BasicPage {
+  myForm = new FormGroup({
+    name: new FormControl<string>(''),
+    price: new FormControl<number>(0),
+    inStorage: new FormControl<number>(0),
+  });
+}
