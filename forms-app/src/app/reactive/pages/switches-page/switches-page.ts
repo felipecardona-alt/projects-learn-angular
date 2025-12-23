25bcee7c622cfcbbbba5a5d3d4c7b96d23c85781
felipe.cardona <felipe.cardona@ceiba.com.co>
2025-12-23 10:23:33 -0500

    Feat(switches-page): ✨ implement reactive form with validation

--- forms-app/src/app/reactive/pages/switches-page/switches-page.ts
+++ forms-app/src/app/reactive/pages/switches-page/switches-page.ts
@@ -1,9 +1,26 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/forms-utils';
 
 @Component({
-  selector: 'app-switches-page',
-  imports: [JsonPipe],
+  selector: 'switches-page',
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-page.html',
 })
-export class SwitchesPage { }
+export class SwitchesPage {
+  private formBuilder = inject(FormBuilder);
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this.formBuilder.group({
+    gender: ['M', Validators.required],
+    wathNotifications: [true],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+    console.log(this.myForm.value);
+  }
+
+}
