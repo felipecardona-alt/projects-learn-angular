80161ad3cfa16936491fe0a6f66c168892e2c194
felipe.cardona <felipe.cardona@ceiba.com.co>
2025-12-24 11:21:27 -0500

    Feat(home-page): ✨ implement lifecycle hooks and reactive properties

--- lifecyclehooks/src/app/app.config.ts
+++ lifecyclehooks/src/app/app.config.ts
@@ -1,4 +1,5 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners,
+   provideZoneChangeDetection, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,7 +7,8 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    // provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideZonelessChangeDetection(),
   ]
 };
--- lifecyclehooks/src/app/pages/home-page/home-page.ts
+++ lifecyclehooks/src/app/pages/home-page/home-page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { afterEveryRender, afterNextRender, Component, effect, OnChanges, OnDestroy, OnInit, signal } from '@angular/core';
+
+const log = (...messages:string[]) => {
+  console.log(`${messages[0] } %c${messages.slice(1).join(' ')}`, 'color: #17cf30ff; font-weight: bold;');
+}
 
 @Component({
   selector: 'home-page',
@@ -6,4 +10,68 @@ import { Component } from '@angular/core';
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
-export class HomePage { }
+export class HomePage implements OnInit, OnChanges, OnDestroy{
+
+  traditionalProperty = 'Felipe';
+  signalProperty = signal('Felipe');
+
+  changeTraditional() {
+    this.traditionalProperty = 'Felipe Cardona';
+  }
+
+  changeSignal() {
+    this.signalProperty.set('Felipe Cardona');
+  }
+
+
+  constructor() {
+    console.log('Constructor  llamado');
+
+    setTimeout(() => {
+      this.changeSignal();
+      // this.changeTraditional();
+      console.log('Traditional property changed hecho');
+    }, 1000);
+  }
+
+  basicEffect = effect((onCleanup) => {
+    log('Effect','Runs once when created and then every time any reactive dependency changes.');
+
+    onCleanup(() => {
+      log('Effect Cleanup','Runs before the effect re-runs or when the component is destroyed.');
+    });
+  });
+
+  ngOnDestroy(): void {
+    log('NgOnDestroy','Runs just before Angular destroys the component.');
+  }
+  ngOnInit() {
+    log('NgOnInit','Runs once after Angular has initialized all the component\'s inputs.');
+  }
+  ngOnChanges(){
+    log('NgOnChanges','Runs every time the component\'s inputs have changed.');
+  }
+  ngDoCheck(){
+    log('NgDoCheck','Runs every time this component is checked for changes.');
+  }
+  ngAfterContentInit(){
+    log('NgAfterContentInit','Runs once after the component\'s content has been initialized.');
+  }
+  ngAfterContentChecked(){
+    log('NgAfterContentChecked','Runs every time this component content has been checked for changes.');
+  }
+  ngAfterViewInit(){
+    log('NgAfterViewInit','Runs once after the component\'s view has been initialized.');
+  }
+  ngAfterViewChecked(){
+    log('NgAfterViewChecked','Runs every time the component\'s view has been checked for changes.');
+  }
+
+  afterNextRenderEffect = afterNextRender(() => {
+    log('afterNextRender','Runs after the next render of the component.');
+  });
+
+  afterRenderEffect = afterEveryRender(() => {
+    log('afterEveryRender','Runs after every render of the component.');
+  });
+}
