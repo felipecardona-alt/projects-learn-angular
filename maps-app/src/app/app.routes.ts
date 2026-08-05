854079a2b2586fdb0742c61666eec9b5cce1661f
felipecardona-alt <felipe.cardona@ceiba.com.co>
2026-08-05 12:49:09 -0500

    Feat(maps-app): ✨ add routes in navbar

--- maps-app/src/app/app.routes.ts
+++ maps-app/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { FullscreenMapPage } from './pages/fullscreen-map-page/fullscreen-map-page';
 
 export const routes: Routes = [
-
     {
         path: 'fullscreen',
         component: FullscreenMapPage,
--- maps-app/src/app/shared/components/navbar/navbar.ts
+++ maps-app/src/app/shared/components/navbar/navbar.ts
@@ -1,9 +1,40 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, inject, Type } from '@angular/core';
+import { routes } from '../../../app.routes';
+import { NavigationEnd, Resolve, ResolveFn, Router, RouterLink } from '@angular/router';
+import { filter, map, Observable, tap } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [AsyncPipe, RouterLink],
   templateUrl: './navbar.html',
-  changeDetection: ChangeDetectionStrategy.Eager,
 })
-export class Navbar {}
+export class Navbar {
+
+  router = inject(Router);
+
+  routes = routes.map( route => ({
+    path: route.path,
+    title: `${route.title ?? 'Maps en Angular'}`,
+  })).filter(route => route.path !== '**');
+
+  pageTitle$ = this.router.events.pipe(
+    filter(event => event instanceof NavigationEnd),
+   // tap((event) => console.log('Router event:', event)),
+    map((event) => event.url),
+    map(
+      (url) => routes.find(route => `/${route.path}` === url)?.title ?? 'Maps')
+  );
+
+  pageTitle = toSignal(
+    this.router.events.pipe(
+    filter(event => event instanceof NavigationEnd),
+   // tap((event) => console.log('Router event:', event)),
+    map((event) => event.url),
+    map(
+      (url) => routes.find(route => `/${route.path}` === url)?.title ?? 'Maps')
+    )
+  );
+
+}
