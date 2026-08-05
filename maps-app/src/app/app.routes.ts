99e2b340c6b35fb05c5db5a1963f7a22dc9ef142
felipecardona-alt <felipe.cardona@ceiba.com.co>
2026-08-05 12:03:05 -0500

    Feat(maps-app) :✨ init project

---
+++ maps-app/src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+import { FullscreenMapPage } from './pages/fullscreen-map-page/fullscreen-map-page';
+
+export const routes: Routes = [
+
+    {
+        path: 'fullscreen',
+        component: FullscreenMapPage,
+        title: 'FullScreen Map'
+    },
+    {
+        path: 'markers',
+        component: FullscreenMapPage,
+        title: 'Marcadores'
+    },
+    {
+        path: 'houses',
+        component: FullscreenMapPage,
+        title: 'Propiedades disponibles'
+    },
+    {
+        path: '**',
+        redirectTo: 'fullscreen,'
+    }
+];
---
+++ maps-app/src/app/pages/house-page/house-page.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-house-page',
+  imports: [],
+  templateUrl: './house-page.html',
+  styleUrl: './house-page.scss',
+  changeDetection: ChangeDetectionStrategy.Eager,
+})
+export class HousePage {}
